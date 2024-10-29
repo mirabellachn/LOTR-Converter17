63212b31c4c12b8240c9ef3cb9c1084a67baaa9a
mirabellachn <mirabellachn@gmail.com>
2024-10-29 21:32:41 +0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by mirabella  on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
