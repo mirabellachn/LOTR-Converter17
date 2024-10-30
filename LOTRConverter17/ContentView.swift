7002b888d0b3269857da9beb9822e760421b7269
mirabellachn <mirabellachn@gmail.com>
2024-10-30 15:18:04 +0700

    text field and binding

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
    @State var showExchangeInfo = false
+   @State var leftAmount = ""
+   @State var rightAmount = ""
     
     
     var body: some View {
@@ -48,9 +50,11 @@ struct ContentView: View {
                                 .foregroundStyle(.white)
                             
                         }
+                        .padding(.bottom, -5)
                         
                         // Text field
-                        Text("TextField")
+                        TextField("Amount", text: $leftAmount)
+                            .textFieldStyle(.roundedBorder)
                     }
                     
                     // Equal sign
@@ -76,11 +80,17 @@ struct ContentView: View {
                                 .scaledToFit()
                                 .frame(height: 33)
                         }
+                        .padding(.bottom, -5)
                         
                         // Text field
-                        Text("TextField")
+                        TextField("Amount", text: $rightAmount)
+                            .textFieldStyle(.roundedBorder)
+                            .multilineTextAlignment(.trailing)
                     }
                 }
+                .padding()
+                .background(.black.opacity(0.5))
+                .clipShape(.capsule)
                 
                 Spacer()
                 
